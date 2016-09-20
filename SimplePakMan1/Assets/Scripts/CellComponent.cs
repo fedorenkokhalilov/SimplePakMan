9893f7cbc10a8203e21f1ad308b8c5a1d35c89d9
User-ПК\User <fedorenkokhalilov@gmail.com>
2016-09-20 14:36:33 +0300

    1 part

---
+++ SimplePakMan1/Assets/Scripts/CellComponent.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class CellComponent : MonoBehaviour {
+
+	public event Action<int, int> OnItemClic;
+
+	private void OnItemClicHandler()
+	{
+		if (OnItemClic != null)
+			OnItemClic (Row, Column);
+	}
+
+	private float _delay = 0.25f;
+
+			public int Row {
+				get;
+				set;
+			}
+
+			public int Column {
+				get;
+				set;
+			}
+
+	[SerializeField]
+	private Image _image;
+
+	[SerializeField]
+	private Color _selectedColor;
+
+	private Color _normalColor;
+
+
+	public void OnSelect(){
+		OnItemClicHandler ();	}
+
+	bool _isSelected;
+	public bool ISSelected{
+		set{ 
+			_isSelected = value;
+			CancelInvoke ();
+			if (value)
+				InvokeRepeating ("SwitchColor", 0, _delay);
+			else if (_image) {
+				_image.color = _normalColor;
+			}
+		}
+		get{ 
+			return _isSelected;
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		_normalColor = _image.color;
+	}
+	
+	// Update is called once per frame
+
+
+	private void SwitchColor()
+	{
+		if (!_image)
+			return;
+		_image.color = _image.color== _normalColor ? _selectedColor : _normalColor;
+	}
+}
---
+++ SimplePakMan1/Assets/Scripts/GameController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField]
+	private CellComponent _prefab;
+	[SerializeField]
+	private Transform _wrapper;
+
+	private int _rowCount=10;
+	private int _columnCount=10;
+	private int _prevSelRow;
+	private int _prevSelColumn;
+
+	private CellComponent[,] _field;
+
+
+	void Start () {
+		GenerateField ();
+	}
+
+	private void LateUpdate()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			_field [_prevSelRow, _prevSelColumn].ISSelected = false;
+			_prevSelRow--;
+			_prevSelRow=_prevSelRow<0 ?_field.GetLength(0)-1 : _prevSelRow;
+			_field [_prevSelRow, _prevSelColumn].ISSelected = true;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			_field [_prevSelRow, _prevSelColumn].ISSelected = false;
+
+			_prevSelRow++;
+			_prevSelRow=_prevSelRow>= _field.GetLength(0)?0:_prevSelRow;
+			_field [_prevSelRow, _prevSelColumn].ISSelected = true;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			_field [_prevSelRow, _prevSelColumn].ISSelected = false;
+			_prevSelColumn++;
+			_prevSelColumn=_prevSelColumn>=_field.GetLength(1)-1? 0 : _prevSelColumn;
+			_field [_prevSelRow, _prevSelColumn].ISSelected = true;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			_field [_prevSelRow, _prevSelColumn].ISSelected = false;
+
+			_prevSelColumn--;
+			_prevSelColumn=_prevSelColumn <0 ?_field.GetLength(1)-1:_prevSelColumn;
+			_field [_prevSelRow, _prevSelColumn].ISSelected = true;
+		}
+	}
+
+	void GenerateField () {
+		if (!_prefab || !_wrapper)
+			return;
+		_field = new CellComponent[_rowCount, _columnCount];
+		for (int i = 0; i < _rowCount; i++) {
+			for (int j = 0; j < _columnCount; j++) {
+				var item =  _prefab.GetInstance();
+				item.transform.SetParent (_wrapper, false);
+				_field [i, j] = item;
+				item.gameObject.name = string.Format ("{0},{1}", i, j);
+
+				item.Row = i;
+				item.Column = j;
+				item.OnItemClic+= Item_OnItemClic;
+			}
+		}
+		}
+
+	void Item_OnItemClic (int row, int column)
+	{
+		Debug.LogFormat ("Row {0}, Column {1}", row, column);
+		_field [_prevSelRow, _prevSelColumn].ISSelected = false;
+		_field [row, column].ISSelected = true;
+		_prevSelRow = row;
+		_prevSelColumn = column;
+	}
+	}
